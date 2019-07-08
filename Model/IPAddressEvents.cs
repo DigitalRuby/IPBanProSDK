68b31cfb86b7bbfee2fbbe830a6a5f12c8efbd0e
Jeff Johnson <jjxtra@gmail.com>
2019-07-08 11:27:49 -0600

    New model object for ip address events

---
+++ Model/IPAddressEvents.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+using Newtonsoft.Json;
+
+namespace DigitalRuby.IPBanProSDK
+{
+    public class IPAddressEvents
+    {
+        public IPAddressEntry IPAddress { get; set; }
+        public IReadOnlyList<FailedLoginAttempt> FailedLoginAttempts;
+        public IReadOnlyList<SuccessLoginAttempt> SuccessLoginAttempts;
+
+        [DisplayFormat(ConvertEmptyStringToNull = false)]
+        [Required(AllowEmptyStrings = true)]
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public bool Banned { get; set; }
+    }
+}
