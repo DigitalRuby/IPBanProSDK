d2956b519378b5965bb13e69d23532580e6f42fb
Jeff Johnson <jjxtra@gmail.com>
2019-07-23 11:06:02 -0600

    New settings listener interface

---
+++ Interfaces/ISettingsChangedListener.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DigitalRuby.IPBanProSDK
+{
+    /// <summary>
+    /// Handler for settings changes
+    /// </summary>
+    public interface ISettingsChangedListener
+    {
+        /// <summary>
+        /// Notify that settings have changed
+        /// </summary>
+        void SettingsChanged(Settings newSettings);
+    }
+}
