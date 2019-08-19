c4a82fd12e47b5b4bc0c68d27f18c40671ecd44f
Jeff Johnson <jjxtra@gmail.com>
2019-08-19 13:26:29 -0600

    Tweak truncate to be more clear

--- IPBanProSDKExtensionMethods.cs
+++ IPBanProSDKExtensionMethods.cs
@@ -155,10 +155,9 @@ namespace DigitalRuby.IPBanProSDK
         /// <returns>Truncated string or original string if no truncation</returns>
         public static string Truncate(this string s, int maxLength)
         {
-            maxLength--;
-            if (s != null && maxLength >= 0 && s.Length > maxLength)
+            if (s != null && maxLength > 0 && s.Length > maxLength)
             {
-                s = s.Substring(0, maxLength) + "…";
+                s = s.Substring(0, maxLength - 1) + "…";
             }
             return s;
         }
