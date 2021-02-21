1a8eec5efcaee23d13e2fd34c6069a8379532e72
Jeff Johnson <jjxtra@gmail.com>
2021-02-20 18:47:48 -0700

    New properties

--- Model/LicensesModel.cs
+++ Model/LicensesModel.cs
@@ -13,34 +13,10 @@ namespace DigitalRuby.IPBanProSDK
     public class LicensesModel : BaseModel
     {
         /// <summary>
-        /// License keys, newline (\n) delimited.
+        /// License key info, newline (\n) delimited.
         /// </summary>
         [DataMember(Order = 1)]
         [LocalizedDisplayName(nameof(IPBanResources.Licenses))]
         public string Licenses { get; set; }
-
-        /// <summary>
-        /// License keys used
-        /// </summary>
-        [DataMember(Order = 2)]
-        [LocalizedDisplayName(nameof(IPBanResources.Used))]
-        public int Used { get; set; }
-
-        /// <summary>
-        /// License keys available
-        /// </summary>
-        [DataMember(Order = 3)]
-        [LocalizedDisplayName(nameof(IPBanResources.Available))]
-
-        public int Available { get; set; }
-
-        /// <summary>
-        /// Whether to remove all existing uses of license keys and reset usage
-        /// This can be done if you had an old machine using license keys that you
-        /// no longer need
-        /// </summary>
-        [DataMember(Order = 4)]
-        [LocalizedDisplayName(nameof(IPBanResources.RemoveAllLicenseKeys))]
-        public bool RemoveAll { get; set; }
     }
 }
