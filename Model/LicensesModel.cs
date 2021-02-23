150dd8b4fb527b20c0c05fd57441c26d2661501e
Jeff Johnson <jjxtra@gmail.com>
2021-02-23 15:02:32 -0700

    New property

--- Model/LicensesModel.cs
+++ Model/LicensesModel.cs
@@ -18,5 +18,12 @@ namespace DigitalRuby.IPBanProSDK
         [DataMember(Order = 1)]
         [LocalizedDisplayName(nameof(IPBanResources.Licenses))]
         public string Licenses { get; set; }
+
+        /// <summary>
+        /// Number of connected clients
+        /// </summary>
+        [DataMember(Order = 2)]
+        [LocalizedDisplayName(nameof(IPBanResources.ConnectedClients))]
+        public long ConnectedClients { get; set; }
     }
 }
