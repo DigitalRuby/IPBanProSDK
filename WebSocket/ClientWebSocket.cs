7ac55f566dbddf6af2fabf1849e0a985eb2494f9
Jeff Johnson <jjxtra@gmail.com>
2022-03-29 16:08:52 -0600

    Adjust logging

--- WebSocket/ClientWebSocket.cs
+++ WebSocket/ClientWebSocket.cs
@@ -615,12 +615,8 @@ namespace DigitalRuby.IPBanProSDK
                 }
                 catch (Exception ex)
                 {
-                    if (ex is WebSocketException)
-                    {
-                        IPBanCore.Logger.Debug(ex.ToString());
-                    }
                     // we don't care about these exception types
-                    else if (!(ex is OperationCanceledException || ex is ObjectDisposedException))
+                    if (!(ex is OperationCanceledException || ex is ObjectDisposedException))
                     {
                         // eat exceptions, most likely a result of a disconnect, either way we will re-create the web socket
                         IPBanCore.Logger.Info(ex.ToString());
