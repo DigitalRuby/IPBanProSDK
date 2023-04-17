7b4bc530b8b79bbe03ccae8ea556d050d5b02edd
Jeff Johnson <jjxtra@gmail.com>
2023-04-17 14:42:04 -0600

    New models

---
+++ Model/AutoWhitelist/AutoWhitelistUrl.cs
@@ -0,0 +1,74 @@
+﻿/*
+
+IPBanPro SDK - https://ipban.com | https://github.com/DigitalRuby/IPBanProSDK
+IPBan and IPBan Pro Copyright(c) 2012 Digital Ruby, LLC
+support@ipban.com
+
+The MIT License(MIT)
+
+Copyright(c) 2012 Digital Ruby, LLC
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+*/
+
+using System;
+using System.Collections.Generic;
+
+namespace DigitalRuby.IPBanProSDK.Model.AutoWhitelist
+{
+
+    /// <summary>
+    /// Auto whitelist url
+    /// </summary>
+    public sealed class AutoWhitelistUrl
+    {
+        /// <summary>
+        /// Id
+        /// </summary>
+        public string Id { get; init; }
+
+        /// <summary>
+        /// Expiration timestamp
+        /// </summary>
+        public string Expires { get; init; }
+
+        /// <summary>
+        /// Access duration for each use
+        /// </summary>
+        public TimeSpan AccessDuration { get; init; }
+
+        /// <summary>
+        /// Remaining allowed uses
+        /// </summary>
+        public int RemainingUses { get; init; }
+
+        /// <summary>
+        /// Machine access or null for all
+        /// </summary>
+        public string MachineAccess { get; init; }
+
+        /// <summary>
+        /// IP addresses that have registered with this url
+        /// </summary>
+        public List<UrlIPAddress> IPAddresses { get; init; } = new();
+    }
+
+    /// <summary>
+    /// Url ip address
+    /// </summary>
+    /// <param name="IPAddress">IP address</param>
+    /// <param name="Expires">Expires</param>
+    public record UrlIPAddress(string IPAddress, string Expires)
+    {
+        /// <inheritdoc />
+        public override string ToString()
+        {
+            return $"{IPAddress},{Expires}";
+        }
+    }
+}
