64147125a26d7f81c71cf810d51ad79bbb09331d
Jeff Johnson <jjxtra@gmail.com>
2025-01-28 11:20:02 -0700

    Machine whitelist model

---
+++ Model/MachineWhitelistModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace DigitalRuby.IPBanProSDK
+{
+    /// <summary>
+    /// Machine whitelist model
+    /// </summary>
+    [Serializable]
+    [DataContract]
+    public sealed class MachineWhitelistModel : BaseModel
+    {
+        /// <summary>
+        /// Machine id
+        /// </summary>
+        [DataMember(Order = 1)]
+        public long Id { get; set; }
+
+        /// <summary>
+        /// Allowed IP addresses (comma separated, can include CIDR notation)
+        /// Example: 1.1.1.1,2.2.2.0/24.
+        /// </summary>
+        [DataMember(Order = 2)]
+        public string AllowedIPAddresses { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Allowed ports (comma separated, ranges denoted with -). If set, any port not in this list will be blocked.
+        /// These are independent of the allowed ip addresses, which take precedence.
+        /// Example: 80,443,5000-5050.
+        /// </summary>
+        [DataMember(Order = 3)]
+        public string AllowedPorts { get; set; } = string.Empty;
+    }
+}
